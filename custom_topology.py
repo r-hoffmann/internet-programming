5a7c0b4fe9c79ecd1f0fb5abbfcfc0c3ade5b027
Dirk Zomerdijk <dirkzomerdijk@gmail.com>
2019-10-28 18:13:57 +0100

    Alleen link lukt om een of andere reden niet...

--- custom_topology.py
+++ custom_topology.py
@@ -1,34 +1,40 @@
-"""Custom topology example
-
-Two directly connected switches plus a host for each switch:
-
-   host --- switch --- switch --- host
-
-Adding the 'topos' dict with a key/value pair to generate our newly defined
-topology enables one to pass in '--topo=mytopo' from the command line.
-"""
-
+                                                                                           
 from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
 
-class MyTopo( Topo ):
-    "Simple topology example."
+class multiSwitchTopo(Topo):
+    "Multiple switches (n_switches) connected to multiple hosts (n_hosts) \
+        with eavenly devided number of hosts per switch (hostsperswitch)."
+    def __init__(self, n_switches=2, n_hosts=4, hostsperswitch=2):
 
-    def __init__( self ):
-        "Create custom topo."
-
-        # Initialize topology
         Topo.__init__( self )
+        # switches =  [self.addSwitch('s%s' % (i + 1)) for i in range(n_switches)]
+        # hosts =     [self.addHost('h%s' % (i + 1)) for i in range(n_hosts)]
+        # print(hosts[i])
+        for i in range(n_switches):
+            switch = self.addSwitch('s%s' % (i + 1))
+            for j in range(n_hosts/hostsperswitch):
+                host = self.addHost('h%s' % (i*hostsperswitch+j+1))
+                print(switch, host)
+                # self.addLink(switch, host)
+        # print(switch)
 
-        # Add hosts and switches
-        leftHost = self.addHost( 'h1' )
-        rightHost = self.addHost( 'h2' )
-        leftSwitch = self.addSwitch( 's3' )
-        rightSwitch = self.addSwitch( 's4' )
 
-        # Add links
-        self.addLink( leftHost, leftSwitch )
-        self.addLink( leftSwitch, rightSwitch )
-        self.addLink( rightSwitch, rightHost )
+def simpleTest():
+    "Create and test a simple network"
+    topo = multiSwitchTopo(n_switches=2, n_hosts=4, hostsperswitch=2)
+    net = Mininet(topo)
+    # net.start()
+    # print "Dumping host connections"
+    # dumpNodeConnections(net.hosts)
+    # print "Testing network connectivity"
+    # net.pingAll()
+    # net.stop()
 
 
-topos = { 'mytopo': ( lambda: MyTopo() ) }
+if __name__ == '__main__':
+    # Tell mininet to print useful information
+    setLogLevel('info')
+    simpleTest()
