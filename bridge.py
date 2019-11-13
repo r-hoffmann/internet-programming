25805cae2701cc6723d54bb35080a0265f2a983f
Dirk Zomerdijk <dirkzomerdijk@gmail.com>
2019-11-13 13:24:06 +0100

    bridge topology

--- bridge.py
+++ bridge.py
@@ -4,28 +4,18 @@ from mininet.net import Mininet
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
 
-
-'''
-Run: 
-    sudo python custom_topology.py
-
-Om het netwerk te removen van mininet:
-    sudo mn -c
-'''
 class multiSwitchTopo(Topo):
     "Multiple switches (n_switches) connected to multiple hosts (n_hosts) \
         with eavenly devided number of hosts per switch (hostsperswitch)."
-    def __init__(self, n_switches=2, n_hosts=4, hostsperswitch=2):
-
-        Topo.__init__( self )
+    def build(self, n_switches=2, n_hosts=4, hostsperswitch=2):
         for i in range(n_switches):
             switch = self.addSwitch('s%s' % (i + 1))
             for j in range(n_hosts/hostsperswitch):
                 host = self.addHost('h%s' % (i*hostsperswitch+j+1))
-                print(switch, host)
+                
                 self.addLink(switch, host)
-        self.addLink('s1','s2')
-
+        
+        self.addLink('s1', 's2')
 
 def simpleTest():
     "Create and test a simple network"
@@ -39,7 +29,8 @@ def simpleTest():
     net.stop()
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     # Tell mininet to print useful information
-    setLogLevel('info')
-    simpleTest()
+    # setLogLevel('info')
+    # simpleTest()
+TOPOS = { 'bridge': ( lambda: multiSwitchTopo(n_switches=2, n_hosts=4, hostsperswitch=2) ) }
