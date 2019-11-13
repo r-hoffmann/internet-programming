279322ff45bace7dc6c4e6a46bea0424c2ad0833
Dirk Zomerdijk <dirkzomerdijk@gmail.com>
2019-11-13 15:45:05 +0100

    lab1 learning switch added

---
+++ lab1/learning_switch.py
@@ -0,0 +1,124 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ipv4
+
+
+class LearningSwitch13(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(LearningSwitch13, self).__init__(*args, **kwargs)
+
+        # Initialize mac address table
+        self.mac_to_port = {}
+
+    # Handle the event that switches report to the controller
+    # their feature configurations
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # Install the table-miss flow entry
+        # If a packet cannot be forwarded by matching any of the
+        # entries in the flow-table, this rule allows the packet
+        # to be forwarded to the controller.
+        # Note that the priority is the lowest, i.e., 0.
+
+        # --------------------------------------------------------
+        # Your code starts here
+        # --------------------------------------------------------
+
+        # install the table-miss flow entry.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # construct flow_mod message and send it.
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    # Handle the packet_in event
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # Get Datapath ID to identify OpenFlow switches
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+        
+        # --------------------------------------------------------
+        # Your code starts here: You should first implement the
+        # learning switch logic. The learning switch logic
+        # consists of two components:
+        # * Learn destination MAC and update switch flow table
+        # * Send a packet_out message for the received packet_in
+        # Once the learning switch is running, implement an IP
+        # packet blocker on specified switches to certain
+        # destinations.
+        # --------------------------------------------------------
+        
+        # analyse the received packets using the packet library.
+        pkt = packet.Packet(msg.data)
+        eth_pkt = pkt.get_protocol(ethernet.ethernet)
+        dst = eth_pkt.dst
+        src = eth_pkt.src
+
+
+        blacklist = set(['10.0.0.4'])
+
+        for p in pkt:
+            # print p.protocol_name, p
+            if p.protocol_name == 'ipv4':
+                print p.dst
+                count_packet(p)
+                if p.dst in blacklist:
+                    print('PACKET BLOCKED')
+                    return 
+
+        # get the received port number from packet_in message.
+        in_port = msg.match['in_port']
+        # self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
+
+        # if the destination mac address is already learned,
+        # decide which port to output the packet, otherwise FLOOD.
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD        
+        
+        # construct action list.
+        actions = [parser.OFPActionOutput(out_port)]
+        
+        # install a flow to avoid packet_in next time.
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            self.add_flow(datapath, 1, match, actions)
+
+        # construct packet_out message and send it.
+        out = parser.OFPPacketOut(datapath=datapath,
+                                  buffer_id=ofproto.OFP_NO_BUFFER,
+                                  in_port=in_port, actions=actions,
+                                  data=msg.data)
+        datapath.send_msg(out)
+        
